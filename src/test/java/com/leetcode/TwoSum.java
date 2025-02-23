d41a6e723b2fbf25af5c7b6fed81c2f58af248b1
root <31607926+ha-root@users.noreply.github.com>
2025-02-23 23:09:27 +0900

    1. Tow Sum problem

--- src/test/java/com/leetcode/TwoSum.java
+++ src/test/java/com/leetcode/TwoSum.java
@@ -1,2 +1,28 @@
-package com.leetcode;public class TwoSum {
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    @Test
+    void addition() {
+        int[] data = twoSum(new int[]{2,7,11,15}, 9);
+        System.out.print(data[0]);
+        System.out.print(data[1]);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int pre = target - nums[i];
+            if(map.containsKey(pre)){
+                return new int[]{i, map.get(pre)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
 }
