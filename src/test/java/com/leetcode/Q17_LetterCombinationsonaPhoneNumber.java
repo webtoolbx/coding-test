16aa925f0776acf2dfa2cce22ab4be93f5c05c7c
하재근(224099) <jgha@ssg.com>
2025-08-24 15:12:53 +0900

    temp commit Q17_LetterCombinationsonaPhoneNumber.java

---
+++ src/test/java/com/leetcode/Q17_LetterCombinationsonaPhoneNumber.java
@@ -0,0 +1,64 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class Q17_LetterCombinationsonaPhoneNumber {
+
+    @Test
+    void test() {
+        System.out.println("result : " + maxDepth(new TreeNode(1)));
+    }
+
+    public int maxDepth(TreeNode root) {
+        return cal(root);
+    }
+
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+        list.add(digits);
+        int repeat = digits.length();
+        for(int i = 0; i < repeat; i++){
+            char c = digits.charAt(i);
+            if(c == '2'){
+                // list.add();
+            } else if(c == '3'){
+
+            } else if(c == '4'){
+
+            } else if(c == '5'){
+
+            } else if(c == '6'){
+
+            } else if(c == '7'){
+
+            } else if(c == '8'){
+
+            } else if(c == '9'){
+
+            }
+        }
+
+        for(char c : digits.toCharArray()){
+            if(c == '2'){
+
+            } else if(c == '3'){
+
+            } else if(c == '4'){
+
+            } else if(c == '5'){
+
+            } else if(c == '6'){
+
+            } else if(c == '7'){
+
+            } else if(c == '8'){
+
+            } else if(c == '9'){
+
+            }
+        }
+    }
+}
